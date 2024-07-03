f891e3f89e02ba6e4faeac9e2e9d8f058b9c7bdd
j-okubo <okubo.jun0912@gmail.com>
2024-07-04 00:31:18 +0900

    itemcontrollerの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,expect:[:index,:show,:edit]
+  before_action :authenticate_user!,only:[:new,:show,:edit]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
