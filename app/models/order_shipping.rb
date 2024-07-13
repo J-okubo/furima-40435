01daf7a30e8ee68362b7574491c87ae07a56e225
j-okubo <okubo.jun0912@gmail.com>
2024-07-13 17:15:53 +0900

    order_shippingモデルの編集

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -7,13 +7,13 @@ class OrderShipping
   end
 
   with_options presence: true do
-    validates :token, presence: true
-    validates :item_id, presence: true
-    validates :user_id, presence: true
-    validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :token
+    validates :item_id
+    validates :user_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :street_address, presence: true
-    validates :street_number, presence: true
+    validates :street_address
+    validates :street_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
 
