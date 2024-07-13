5e091b5639e6d98c29b751fa31c40da4380ab5d8
j-okubo <okubo.jun0912@gmail.com>
2024-07-13 16:38:52 +0900

    userModelの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,5 @@ class User < ApplicationRecord
   validates :last_name_han, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters" }
   validates :date_of_birth, presence: true
   has_many :items
-  belongs_to :user
   has_one :order
 end
